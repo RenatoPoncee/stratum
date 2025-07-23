076049f2ca2f8938549fcf72c0fe803ba9fd5304
RenatoPoncee <51792188+RenatoPoncee@users.noreply.github.com>
2025-07-23 12:33:32 -0300

    Modularize sensors

---
+++ balao/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include "lora_e220.h"
+#include "magnetometer.h"
+
+#define PINO_DRDY 2
+#define INTERVALO 120000UL
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  setupE220();
+  setupMagnetometer(PINO_DRDY);
+  sendMessage("Balloon ready");
+}
+
+void loop() {
+  float azimute;
+  if (readMagnetometer(azimute)) {
+    String msg = "azimute=" + String(azimute, 2);
+    sendMessage(msg);
+    Serial.println(msg);
+  }
+
+  if (Serial.available()) {
+    String input = Serial.readStringUntil('\n');
+    sendMessage(input);
+  }
+
+  delay(INTERVALO);
+}
